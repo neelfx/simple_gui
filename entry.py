07a4541868e6212b184e40d3e5819f4af5f5341c
Neel Patel <neel@hotmail.com>
2021-02-21 22:22:06 +0000

    entry field

---
+++ entry.py
@@ -0,0 +1,20 @@
+# Testing buttons on Tkinter
+
+from tkinter import *
+
+root = Tk()
+
+e = Entry(root, width=50, bg="blue", fg="white", borderwidth=15)
+e.pack()
+e.insert(0, "Enter Your Name: ")
+def myClick():
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
+    myLabel.pack()
+
+myButton = Button(root, text="Enter your name", padx=50, pady=50, command=myClick, fg="blue", bg="red")
+myButton.pack()
+
+# Create Event Loop
+root.mainloop()
+
