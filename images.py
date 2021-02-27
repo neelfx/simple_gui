8bf8bd41d5bebd644235d9e4941314744369559c
Neel Patel <neel@hotmail.com>
2021-02-27 22:51:19 +0000

    loading images

---
+++ images.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from PIL import ImageTk, Image
+root =Tk()
+root.title('Adding an icon')
+root.iconbitmap('images/butterfly.ico')
+
+my_img = ImageTk.PhotoImage(Image.open("images/Trees.jpg"))
+my_label = Label(image=my_img)
+my_label.pack()
+
+
+
+
+button_quit = Button(root, text="Exit Program", command=root.quit)
+button_quit.pack()
+
+
+
+
+root.mainloop()
