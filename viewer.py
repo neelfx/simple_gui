5f60dcaaef823aa547bf96a73518208de3a03042
Neel Patel <neel@hotmail.com>
2021-02-28 20:23:56 +0000

    fix bug

--- viewer.py
+++ viewer.py
@@ -12,11 +12,11 @@ my_img3 = ImageTk.PhotoImage(Image.open("images/airline.jpg"))
 my_img4 = ImageTk.PhotoImage(Image.open("images/rising_sun.png"))
 my_img5 = ImageTk.PhotoImage(Image.open("images/koto.jpg"))
 
-global img_pos
-global my_label
-global button_forward
-global button_back
-global image_list
+# global img_pos
+# global my_label
+# global button_forward
+# global button_back
+# global image_list
 
 img_pos = 0
 
@@ -26,18 +26,21 @@ image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
 my_label = Label(image=image_list[img_pos])
 my_label.grid(row=0, column=0, columnspan=3)
 
-def forward(img_pos, my_label, image_list):
-    img_pos = img_pos+1
+def forward():
+    global img_pos, my_label, image_list
+    img_pos += 1
     if img_pos == 5:
         img_pos = 0
-
+    print(img_pos)
     my_label.grid_forget()
     my_label = Label(image=image_list[img_pos])
     my_label.grid(row=0, column=0, columnspan=3)
     
 
-def back(img_pos, my_label, image_list):
-    img_pos = img_pos-1
+def back():
+    global img_pos, my_label, image_list
+    img_pos -= 1
+    print(img_pos)
     if img_pos == -1:
         img_pos = 4
 
@@ -45,9 +48,9 @@ def back(img_pos, my_label, image_list):
     my_label = Label(image=image_list[img_pos])
     my_label.grid(row=0, column=0, columnspan=3)
 
-button_back = Button(root, text="<<", command=lambda: back(img_pos, my_label, image_list))
+button_back = Button(root, text="<<", command=back)
 button_quit = Button(root, text="Exit Program", command=root.quit)
-button_forward = Button(root, text=">>", command=lambda: forward(img_pos, my_label, image_list))
+button_forward = Button(root, text=">>", command=forward)
 
 button_back.grid(row=1, column=0)
 button_quit.grid(row=1, column=1)
