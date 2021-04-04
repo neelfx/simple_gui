fe76d185bf43ebdbe91c02bee874dae8b41612cd
Neel Patel <neel@hotmail.com>
2021-04-04 21:02:24 +0100

    experimenting2

--- buttons.py
+++ buttons.py
@@ -7,7 +7,7 @@ def myClick():
     myLabel = Label(root, text="Look! I click a Button!!!")
     myLabel.pack()
 
-myButton = Button(root, text="Click Me!", padx=50, pady=50, command=myClick, fg="blue", bg="red")
+myButton = Button(root, text="Click Me!", padx=500, pady=500, command=myClick, fg="blue", bg="red")
 myButton.pack()
 
 # Create Event Loop
